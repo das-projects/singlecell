3e2a15084cbd7c14ee6568287851517db0e8a74b
Arijit Das <arijitd@gmail.com>
2021-02-21 12:41:01 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='singlecell',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Accelerated Single Cell Analysis',
+    author='Arijit Das <arijitd@gmail.com>',
+    license='BSD-3',
+)
